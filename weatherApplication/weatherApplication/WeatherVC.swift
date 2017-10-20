8a096798eae5bce1bb086cd6d170c7547a2f72a5
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2017-10-20 11:38:15 +0200

    Implement table view methods

--- weatherApplication/weatherApplication/WeatherVC.swift
+++ weatherApplication/weatherApplication/WeatherVC.swift
@@ -23,3 +23,22 @@ class WeatherVC: UIViewController {
     }
 }
 
+extension WeatherVC: UITableViewDelegate {
+    
+}
+
+extension WeatherVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) 
+            return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+}
+
