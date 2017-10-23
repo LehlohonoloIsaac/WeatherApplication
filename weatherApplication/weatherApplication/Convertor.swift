c25fd631f6d57c1bdc4150e30cd48d3d00142228
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2017-10-23 13:26:15 +0200

    Data modelling and initialization of weather dictionary

---
+++ weatherApplication/weatherApplication/Convertor.swift
@@ -0,0 +1,14 @@
+//
+//  Convertor.swift
+//  weatherApplication
+//
+//  Created by dvt on 2017/10/23.
+//  Copyright © 2017 dvt. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+func convertKevinToDegrees(value: Double) -> Double {
+    return value - 273
+}
